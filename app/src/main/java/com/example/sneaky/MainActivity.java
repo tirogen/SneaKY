bd71b580ae5ca6794b14488cdf48b40f53a72090
Unknown <the-px@hotmail.com>
2019-04-20 02:41:36 +0700

    init storeFragment

--- app/src/main/java/com/example/sneaky/MainActivity.java
+++ app/src/main/java/com/example/sneaky/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.sneaky;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FrameLayout mainFrame;
+    private BottomNavigationView navigation;
+
+    private StoreFragment storeFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +24,14 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_store:
-                    mTextMessage.setText(R.string.title_store);
+                    //mTextMessage.setText(R.string.title_store);
+                    setFragment(storeFragment);
                     return true;
                 case R.id.navigation_cart:
-                    mTextMessage.setText(R.string.title_cart);
+                    //mTextMessage.setText(R.string.title_cart);
                     return true;
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
+                    //mTextMessage.setText(R.string.title_profile);
                     return true;
             }
             return false;
@@ -36,9 +43,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        storeFragment = new StoreFragment();
+
+        mainFrame = (FrameLayout) findViewById(R.id.mainFrame);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrame, fragment);
+        fragmentTransaction.commit();
+    }
 }
